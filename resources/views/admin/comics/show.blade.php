08da09cf92ced483215b81073d5b82600c93959d
Vincedinardo21 <vincedinardo21@gmail.com>
2022-07-19 18:30:52 +0200

    Fix view comics

--- resources/views/admin/comics/show.blade.php
+++ resources/views/admin/comics/show.blade.php
@@ -1,5 +1,11 @@
 @extends('admin.templates.base')
 
 @section('MainContent')
-    <h1>Nome comic: {{$comic->title}}</h1>
+    <div>Titolo: {{$comic->title}}</div>
+    <div>Descrizione: {{$comic->description}}</div>
+    <div>Immagine: <img src="{{$comic->thumb}}" alt=""></div>
+    <div>Prezzo: {{$comic->price}}</div>
+    <div>Serie: {{$comic->series}}</div>
+    <div>Data sconti: {{$comic->sale_date}}</div>
+    <div>Tipo: {{$comic->type}}</div>
 @endsection
